358c3d92abb6f21d1f07e6df32bd220f6f385492
emyeucanha5 <57170354+emyeucanha5@users.noreply.github.com>
2021-09-29 05:34:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const preloader = document.querySelector('.preloader');
-setTimeout(function(){ 
-	preloader.classList.add("hide-preloader")
-}, 600);
-// window.addEventListener('load', function(e) {
+// setTimeout(function(){ 
 // 	preloader.classList.add("hide-preloader")
-// });
+// }, 600);
+window.addEventListener('load', function(e) {
+	preloader.classList.add("hide-preloader")
+});
 const btn = document.querySelector("#main-btn");
 const video = document.querySelector('video');
 btn.addEventListener('click', function() {
