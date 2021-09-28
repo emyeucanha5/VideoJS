b5ebfd9660a6d570147d5d54e03acc270c349a01
emyeucanha5 <57170354+emyeucanha5@users.noreply.github.com>
2021-09-29 05:30:57 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const preloader = document.querySelector('.preloader');
+setTimeout(function(){ 
+	preloader.classList.add("hide-preloader")
+}, 600);
+// window.addEventListener('load', function(e) {
+// 	preloader.classList.add("hide-preloader")
+// });
+const btn = document.querySelector("#main-btn");
+const video = document.querySelector('video');
+btn.addEventListener('click', function() {
+	if(!btn.classList.contains("slide")){
+		video.pause();
+		btn.classList.toggle("slide");
+	}
+	else{
+		video.play();
+		btn.classList.toggle("slide");
+	}
+});
